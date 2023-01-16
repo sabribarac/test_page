99b29f74508ce2871481f498f3f7591f72dd5246
sabribarac <s.barac@umail.leidenuniv.nl>
2023-01-16 13:01:01 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -54,9 +54,12 @@ import re
 import io
 import zipfile
 
+pn.extension()
+pn.extension(sizing_mode="stretch_width", template="fast")
+
 clean_files = []
 clean_filenames = []
-list_regex_default = [('Artikel regel 1' , r"(Artikel=[a-z0-9\.]{1,7})(.{1,})citeertitel=Besluit activiteiten leefomgeving", r"${1}${2}citeertitel=Besluit activiteiten leefomgeving: ${1}"),
+list_regex_default = [('Artikel regel 1' , r"(Artikel=[a-z0-9\.]{1,7})(.{1,})citeertitel=Besluitactiviteiten leefomgeving", r"${1}${2}citeertitel=Besluitactiviteiten leefomgeving: ${1}"),
               ('Artikel regel 2', ": Artikel=", ": artikel "),
               ('Nummers 1', "^3^", "3"),
               ('Nummers 2', "^2^", "2"),
@@ -75,177 +78,62 @@ def on_press_download_button():
         zf.writestr(filename, filecontent)
     zf.close()
     output.seek(0)
-    return output
+    download_button.save(output.getvalue())
 
-def on_add_regex_button(event):
-    # Add string to search_selector
-    list_regex_default.append((textbox_regex_name.value, 
-textbox_regex_from.value, textbox_regex_to.value))
-    # print(list_regex_default)
+def on_add_regex_button():
+    list_regex_default.append((textbox_regex_name.value, textbox_regex_from.value, textbox_regex_to.value))
+    search_selector.options = list_regex_default
+    search_selector.value = list_regex_default
 
-    
-
-#def on_save_button_press(event):
-#    global clean_files
-#    global clean_filenames
-#    import io
-#    import zipfile
-#    output = io.BytesIO()
-#    zf = zipfile.ZipFile(output, mode='w')
-#    zf.writestr(clean_filenames[0], clean_files[0])
-#    zf.writestr(clean_filenames[1], clean_files[1])
-#    zf.close()
-#    open('data.zip', 'wb').write(output.getbuffer())
-#    with open(clean_filenames[0]+'_filter', 'w') as f:
-#        f.writelines(clean_files[0])
-    
+def process_files():
+    global clean_files
+    global clean_filenames
+    clean_files = []
+    clean_filenames = []
+    for file in file_input.value:
+        with open(file, 'r') as f:
+            content = f.read()
+            for regex in search_selector.value:
+                content = re.sub(regex[1], regex[2], content)
+            clean_filenames.append(file.name)
+            clean_files.append(content)
+    download_button.disabled = False
+    progress_gauge.value = 1
 
 # Create a file input component
 file_input = pn.widgets.FileInput(multiple=True)
-#file_input = pn.widgets.FileSelector('~')
 
 # Create a button to start processing the files.
-process_button = Button(name='Process files')
-
-# Create a button to save button that contains the zipped files.
-# save_button = Button(name='Save files')
+process_button = Button(name='Process files', button_type="primary", width=200)
+process_button.on_click(process_files)
 
 # Create download button
-download_button = pn.widgets.FileDownload(filename="data.zip", 
-callback=on_press_download_button, button_type="primary", disabled=True)
+download_button = pn.widgets.FileDownload(filename="data.zip", callback=on_press_download_button, button_type="primary", disabled=True)
 
 # Create a gauge bar to show the progress
-progress_gauge = Gauge(name='Progress', value=0, width=300, title_size=10, 
-colors=[(0.2, 'red'), (0.8, 'gold'), (1, 'green')])
+progress_gauge = Gauge(name='Progress', value=0, width=300, title_size=10, colors=[(0.2, 'red'), (0.8, 'gold'), (1, 'green')])
 
 # Create a crossSelector to search the input in each of the files
-search_selector = CrossSelector(name='Regular Expression', 
-options=list_regex_default, value=list_regex_default, width=1000, 
-definition_order=False)
-
-# Create textbox to show processed files
-textbox = pn.widgets.input.TextAreaInput(placeholder='Processed files are shown here..', width=1000, height=225)
+search_selector = CrossSelector(name='Regular Expression', options=list_regex_default, value=list_regex_default, width=1000, definition_order=False)
 
 # Create textbox to add new regular expressions
-textbox_regex_name = pn.widgets.input.TextAreaInput(placeholder='regex name', width=250, height=50)
-textbox_regex_from = pn.widgets.input.TextAreaInput(placeholder='regex expression', width=250, height=50)
-textbox_regex_to = pn.widgets.input.TextAreaInput(placeholder='replace char', width=250, height=50)
-add_regex_button = Button(name='Add regex', width=180, height=50)
-
-# Create a callback function to handle the button press
-def on_button_press(event):
-    global clean_files
-    global clean_filenames
-    clean_files, clean_filenames = [], []
-    textboxstring = ''
-    progress_gauge.value = 0
-
-    if process_button.clicks:
-        regex = search_selector.value
-        files = file_input.value
-        #print(regex)
-
-        if files is not None:
-            # Iterate over the selected files
-            for i, _ in enumerate(files):
-                string = ''
-                # Update process for files
-                progress_gauge.value = (i + 1) / len(files) * 100
-                # print(file_input.filename[i])
-
-                for reg in regex:
-                    # Do some processing here
-                    #print('Replace %s with %s' %(reg[1], reg[2]))
-                    string =  file_input.value[i].decode('utf-8')
-                    string = re.sub(reg[1], reg[2], string)
-                    #print(string[0:10])
-
-                # Store clean filename
-                clean_files.append(string)
-                clean_filenames.append(file_input.filename[i])
-                textbox.value = ', '.join(clean_filenames)
-                
-                #print(file_input.value[i])
-                #print(file_input.filename[i])
-                # time.sleep(0.1)
-
-            if len(clean_files)>0:
-                download_button.disabled = False
-
-
-# Create the separator line
-separator_vertical = pn.pane.HTML("""
-    <style>
-        .vertical {
-            border-left: 1px solid black;
-            height: 450px;
-            position:absolute;
-            left: 0%;
-        }
-    </style>
-
-<div class= "vertical"></div>
-""")    
-
-
-# Link the button to the callback function
-process_button.on_click(on_button_press)
-
-# Link the regex button to the callback function
+textbox_regex_name = pn.widgets.TextInput(placeholder='Name')
+textbox_regex_from = pn.widgets.TextInput(placeholder='From')
+textbox_regex_to = pn.widgets.TextInput(placeholder='To')
+add_regex_button = Button(name='Add', button_type="primary", width=200)
 add_regex_button.on_click(on_add_regex_button)
 
-# Link the button to the callback function
-# save_button.on_click(on_save_button_press)
+# Create textbox to show processed files
+textbox = pn.widgets.TextAreaInput(placeholder='Processed files are shown here..', width=1000, height=225)
+
+# Create the layout
+layout = pn.Column(file_input, process_button, search_selector, textbox_regex_name, textbox_regex_from, textbox_regex_to, add_regex_button, progress_gauge, textbox, download_button)
+
+# Show the UI
+#layout.show()
+layout.servable()
 
 
-# Create the left-side panel
-left_panel = pn.Column(
-    "Upload files",
-    file_input,
-    pn.Spacer(width=50),  # Add some space between the panels
-    process_button,
-    progress_gauge,
-    # save_button,
-    download_button,
-)
-
-
-# Create the main top panel
-top_panel = pn.Column()
-
-
-# Create the main panel
-main_panel = pn.Column(
-    top_panel,    
-    search_selector,
-    pn.Row(textbox_regex_name, textbox_regex_from, textbox_regex_to, 
-add_regex_button),
-    pn.Row(textbox),
-)
-
-
-# Create the main dashboard
-dashboard = pn.Row(
-    left_panel,
-    pn.Spacer(width=20),  # Set the left panel width to 20%
-    separator_vertical,
-    pn.Spacer(width=50),  # Add some space between the panels
-    main_panel,
-    pn.Spacer(width=10),  # Set the top panel width to 30%
-)
-
-# pn.state.template.param.update(site_url="https://awesome-panel.org",
-#     site="Awesome Panel",
-#     title="Hello World",favicon="https://raw.githubusercontent.com/MarcSkovMadsen/awesome-panel-assets/320297ccb92773da099f6b97d267cc0433b67c23/favicon/ap-1f77b4.ico",)
-# Serve the dashboard
-# dashboard.servable()       
-#pn.extension()
-#pn.extension(sizing_mode="stretch_width", template="fast")
-# panel serve hvplot_interactive.ipynb
-# panel serve --show --autoreload
-
-
-dashboard.servable()
 
 await write_doc()
   `
